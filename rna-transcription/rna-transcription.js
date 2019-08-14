95361deb0d8ed2e40272e698d84b23f6f01a6409
Daniel <daniel@test.no>
2019-08-14 17:27:27 +0200

    New version of transcription

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,14 +1,48 @@
-var Strands = new Map();
-Strands.set("C", "G" );
-Strands.set("G", "C" );
-Strands.set("A", "U" );
-Strands.set("T", "A" );
-
+var Strands = new Map([["C", "G"], ["G", "C"], ["A", "U"], ["T", "A"]] );
 export const toRna = (input = "") =>  { 
   
+ /* let rnavalue = [];
+  [... input].forEach(character => {  rnavalue.push ( Strands.get(character) );   });
+  return rnavalue.join("");  */
+
+  
+  let rnavalue = [];
+  //input.split("").map(a => rnavalue.push ( Strands.get(a) ));
+  input.split("").map(a => rnavalue.push ( Strands.get(a) ));
+  return rnavalue.join("");
+
+
+
+
+
+
+
+
+  /*[...input].forEach(char => {
+    // do something
+    console.log(char);
+  });*/
+
+
+    
+    //rnavalue.push ( Strands.get(value) );
+  }
+
+
+
+//input.forEach ( test => console.log(test));
+
+
+
+
+   /*
    let rnavalue = [];
    for (let value of input) {     
      rnavalue.push ( Strands.get(value) );     
   }
   return rnavalue.join("");  
-}
+  */
+
+
+
+
