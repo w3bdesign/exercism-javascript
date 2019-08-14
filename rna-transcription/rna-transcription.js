5a804cf6f7c74e33fb63c6f713c52b8f1f8bc664
Daniel <daniel@test.no>
2019-08-15 00:22:19 +0200

    Using map()

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,14 +1,5 @@
-var Strands = new Map([["C", "G"], ["G", "C"], ["A", "U"], ["T", "A"]] );
+const Strands = new Map([["C", "G"], ["G", "C"], ["A", "U"], ["T", "A"]] );
 export const toRna = (input = "") =>  { 
-
- /* THIS SEEMS TO WORK TOO: */
- /*
- let rnavalue = [];
-  [... input].forEach(character => {  rnavalue.push ( Strands.get(character) ); });
-  return rnavalue.join("");  
-  */
-  
-  let rnavalue = [];  
-  input.split("").map(char => rnavalue.push ( Strands.get(char) ));
-  return rnavalue.join("");  
-  }
+const x = [... input].map( char =>  Strands.get(char) );
+return x.join("");
+}
