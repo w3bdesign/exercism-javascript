2ff8ca793a460ffe808dbccb7df1b283fdeea4a8
Daniel <daniel@test.no>
2019-08-14 17:27:50 +0200

    New version of transcription

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,48 +1,14 @@
 var Strands = new Map([["C", "G"], ["G", "C"], ["A", "U"], ["T", "A"]] );
 export const toRna = (input = "") =>  { 
-  
- /* let rnavalue = [];
-  [... input].forEach(character => {  rnavalue.push ( Strands.get(character) );   });
-  return rnavalue.join("");  */
 
-  
-  let rnavalue = [];
-  //input.split("").map(a => rnavalue.push ( Strands.get(a) ));
-  input.split("").map(a => rnavalue.push ( Strands.get(a) ));
-  return rnavalue.join("");
-
-
-
-
-
-
-
-
-  /*[...input].forEach(char => {
-    // do something
-    console.log(char);
-  });*/
-
-
-    
-    //rnavalue.push ( Strands.get(value) );
-  }
-
-
-
-//input.forEach ( test => console.log(test));
-
-
-
-
-   /*
-   let rnavalue = [];
-   for (let value of input) {     
-     rnavalue.push ( Strands.get(value) );     
-  }
+ /* THIS SEEMS TO WORK TOO: */
+ /*
+ let rnavalue = [];
+  [... input].forEach(character => {  rnavalue.push ( Strands.get(character) ); });
   return rnavalue.join("");  
   */
-
-
-
-
+  
+  let rnavalue = [];  
+  input.split("").map(char => rnavalue.push ( Strands.get(char) ));
+  return rnavalue.join("");  
+  }
