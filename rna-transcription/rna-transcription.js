384c3145d5ea00a82ee5b30b3a4898cafdd1f053
Daniel <daniel@test.no>
2019-08-15 02:01:27 +0200

    Optimized code

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,5 +1,4 @@
 const Strands = new Map([["C", "G"], ["G", "C"], ["A", "U"], ["T", "A"]] );
-export const toRna = (input = "") =>  { 
-const x = [... input].map( char =>  Strands.get(char) );
-return x.join("");
-}
+    export const toRna = (input = "") =>  {
+        return ([... input].map( char =>  Strands.get(char)).join(""));
+    }
