4d468a92f674c3fa72cf4217a0da9a266f74d5ea
Daniel <daniel@test.no>
2019-08-15 20:10:08 +0200

    First implementation

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,17 +1,13 @@
-const age = (planet, seconds) => {
-  const planets = {
-	'earth': 1,
-	'mercury': 0.2408467,
-	'venus': 0.61519726,
-	'mars': 1.8808158,
-	'jupiter': 11.862615,
-	'saturn': 29.447498,
-	'uranus': 84.016846,
-	'neptune': 164.79132
-  };
-
-  let age = seconds / 31557600 / planets[planet]
-  return age;
+export const age = (planetname, age) => {
+  switch(planetname) {
+    case 'earth':   return Math.round((age/31557600)*100)/100;
+    case 'mercury': return Math.round((age/31557600/0.2408467)*100)/100;
+    case 'venus':   return Math.round((age/31557600/0.61519726)*100)/100;
+    case 'mars':    return Math.round((age/31557600/1.8808158)*100)/100;
+    case 'jupiter': return Math.round((age/31557600/11.862615)*100)/100;
+    case 'saturn':  return Math.round((age/31557600/29.447498)*100)/100;
+    case 'uranus':  return Math.round((age/31557600/84.016846)*100)/100;
+    case 'neptune': return Math.round((age/31557600/164.79132)*100)/100;
+ 
+  }
 };
-
-export { age };
