a371f3be2dbf04e18149cfd19918ea5fb2643270
Daniel <daniel@test.no>
2019-08-15 20:03:04 +0200

    Update space-age.js

--- space-age/space-age.js
+++ space-age/space-age.js
@@ -1,26 +1,17 @@
-export class SpaceAge {
-    constructor(seconds) {
-        this.seconds = seconds;
-    }
-    /*
-    planets = {
-        Earth: 31557600,
-        Mercury: 0.2408467,
-        Venus: 0.61519726,
-        Mars: 1.8808158,
-        Jupiter: 11.862615,
-        Saturn: 29.447498,
-        Uranus: 84.016846,
-        Neptune: 164.79132, 
-    }
-    */
-    
-    onEarth (){return Math.round((this.seconds/31557600)*100)/100;}
-    onMercury (){return Math.round((this.seconds/31557600/0.2408467)*100)/100;}
-    onVenus (){return Math.round((this.seconds/31557600/0.61519726)*100)/100;}
-    onMars (){return Math.round((this.seconds/31557600/1.8808158)*100)/100;}
-    onJupiter (){return Math.round((this.seconds/31557600/11.862615)*100)/100;}
-    onSaturn (){return Math.round((this.seconds/31557600/29.447498)*100)/100;}
-    onUranus() {return Math.round((this.seconds/31557600/84.016846)*100)/100;}
-    onNeptune (){return Math.round((this.seconds/31557600/164.79132)*100)/100;}
-}
+const age = (planet, seconds) => {
+  const planets = {
+	'earth': 1,
+	'mercury': 0.2408467,
+	'venus': 0.61519726,
+	'mars': 1.8808158,
+	'jupiter': 11.862615,
+	'saturn': 29.447498,
+	'uranus': 84.016846,
+	'neptune': 164.79132
+  };
+
+  let age = seconds / 31557600 / planets[planet]
+  return age;
+};
+
+export { age };
