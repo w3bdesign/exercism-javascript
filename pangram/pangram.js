d524c7520b10e83625ed645945234bb21050edbc
Daniel <daniel@test.no>
2019-08-20 01:23:46 +0200

    First

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,6 +1,7 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isPangram = () => {};
+export const isPangram = characters => {
+  let letters = "abcdefghijklmnopqrstuvwxyz";
+  let string = characters.toLowerCase().replace(/[^a-z]/g, "");
+  for (var i = 0; i < letters.length; i++)
+    if (string.indexOf(letters[i]) < 0) return false;
+  return true;
+};
