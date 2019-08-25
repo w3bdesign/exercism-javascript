0ca58d1f99bd2fe221267a28e0d46bf4ce57acbf
Daniel <daniel@test.no>
2019-08-25 18:41:12 +0200

    First commit

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,13 +1,17 @@
 export class Matrix {
-  constructor() {
-    throw new Error("Remove this statement and implement this function");
+  constructor(input) {
+    this.matrix = input
+      .split("\n")
+      .map(row => row.split(" ").map(row => parseInt(row)));
   }
 
   get rows() {
-    throw new Error("Remove this statement and implement this function");
+    return this.matrix;
   }
 
   get columns() {
-    throw new Error("Remove this statement and implement this function");
+    return this.rows[0].map((value, index) => this.rows.map(row => row[index]));
   }
 }
+
+new Matrix("1");
