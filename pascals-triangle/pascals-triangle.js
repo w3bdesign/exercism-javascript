1585a31bb6fcac882d6c9faa5bfa70c899f52b98
Daniel <daniel@test.no>
2019-09-23 17:55:50 +0200

    Begin

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,18 +1,13 @@
-//
-// This is only a SKELETON file for the 'Pascals Triangle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class Triangle {
   constructor() {
-    throw new Error("Remove this statement and implement this function");
+    //
   }
 
   get lastRow() {
-    throw new Error("Remove this statement and implement this function");
+    return 1;
   }
 
   get rows() {
-    throw new Error("Remove this statement and implement this function");
+    return 1;
   }
 }
