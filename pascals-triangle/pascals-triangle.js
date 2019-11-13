454e632c43280ec8e746167f8cf02abb0d868a9d
w3bdesign <daniel@test.no>
2019-11-13 01:02:53 +0100

    First commit of solution for Pascals triangle

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,14 +1,36 @@
 export class Triangle {
-  constructor(triangleSize) {
-    this.size = triangleSize;
-    this.rows = [];    
+  constructor(rowCount) {
+    this.triangleRows = this.setRows(rowCount);
   }
 
   get lastRow() {
-    return [1];
+    return this.triangleRows[this.triangleRows.length - 1];
   }
 
   get rows() {
-    return [[1]];
+    return this.triangleRows;
+  }
+  setRows(rowCount) {
+    const computedRows = [[1]];
+
+    for (let i = 1; i < rowCount; i++) {
+      let curRow = this.computeCurRow(computedRows[i - 1]);
+      computedRows.push(curRow);
+    }
+    return computedRows;
+  }
+
+  computeCurRow(row) {
+    const curRow = [];
+    for (let i = 0; i <= row.length; i++) {
+      if (i === 0) {
+        curRow.push(row[0]);
+      } else if (i === row.length) {
+        curRow.push(row[row.length - 1]);
+      } else {
+        curRow.push(row[i] + row[i - 1]);
+      }
+    }
+    return curRow;
   }
 }
