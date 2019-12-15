ffb0774737ca2a4d5ce6ccfda4b3838e8d48d649
w3bdesign <hijack_xxx@yahoo.com>
2019-12-15 20:30:38 +0100

    Submitted collatz conjecture

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,8 +1,9 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const collatz = (n) => {
+  if (n === 1) return 0;
+  return collatz(n % 2 === 0 ? n / 2 : n * 3 + 1) + 1;
+}
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = (n) => {
+  if (n < 1) throw new Error('Only positive numbers are allowed');
+  return collatz(n);
 };
