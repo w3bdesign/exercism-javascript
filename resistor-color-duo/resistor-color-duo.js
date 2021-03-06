0081fe089c8fb8fcb8b85daa23bfcfe06ad99d34
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-06 01:35:05 +0100

    Rearrange order and use Prettier

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,14 @@
-const colorCode = (color) => COLORS.indexOf(color)
-export const value = (colors) => Number(colors.map(colorCode).join(''))
-export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+export const value = (colors) => Number(colors.map(colorCode).join(''));
+const colorCode = (color) => COLORS.indexOf(color);
