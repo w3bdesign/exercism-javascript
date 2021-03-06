6e6b693231bba7ea044e1fea12db87e5dd462442
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-06 01:33:57 +0100

    Add curly brackets

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -1,7 +1,9 @@
-export const isPangram = characters => {
-  let letters = "abcdefghijklmnopqrstuvwxyz";
-  let string = characters.toLowerCase().replace(/[^a-z]/g, "");
+export const isPangram = (characters) => {
+  let letters = 'abcdefghijklmnopqrstuvwxyz';
+  let string = characters.toLowerCase().replace(/[^a-z]/g, '');
   for (var i = 0; i < letters.length; i++)
-    if (string.indexOf(letters[i]) < 0) return false;
+    if (string.indexOf(letters[i]) < 0) {
+      return false;
+    }
   return true;
 };
