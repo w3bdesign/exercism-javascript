b787fcb13f754dcd1bf5d4bdd02a2b4f26129c0f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-03-06 01:27:58 +0100

    Add curly braces
    Curly braces around statements make for more readable code and help prevent bugs when you add further statements.

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,9 +1,13 @@
 const collatz = (n) => {
-  if (n === 1) return 0;
+  if (n === 1) {
+    return 0;
+  }
   return collatz(n % 2 === 0 ? n / 2 : n * 3 + 1) + 1;
-}
+};
 
 export const steps = (n) => {
-  if (n < 1) throw new Error('Only positive numbers are allowed');
+  if (n < 1) {
+    throw new Error('Only positive numbers are allowed');
+  }
   return collatz(n);
 };
